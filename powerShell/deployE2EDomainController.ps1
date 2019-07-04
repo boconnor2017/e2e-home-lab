d069dc077abaa768f0da32149fc3a1cbf2aa012f
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2019-07-04 10:44:17 -0700

    Update deployE2EDomainController.ps1

--- powerShell/deployE2EDomainController.ps1
+++ powerShell/deployE2EDomainController.ps1
@@ -4,40 +4,24 @@
 #                      is not supported by VMWare, any results of this script are solely for lab
 #                      environments. Use at your own risk. 
 # Date: June 2019
-#
-########################################################################################
-########################  ENTER INPUT PARAMS BEFORE RUNNING SCRIPT  ####################
-########################################################################################
-# Input Parameters
-$connect_esxi_host = ''
-$connect_esxi_user = 'root'
-$connect_esxi_pass = ''
 
-$deployto_datastore = 'e2e-esxi01-ds01'
-$deployto_network = 'VM Network'
+# Input Parameters from Configuration File
+. "C:\e2e-home-lab-master\e2e-home-lab-master\powerShell\_e2e-lab-configuration-parameters.ps1"
 
-$ovf_source = 'C:\e2e-home-lab-master\e2e-home-lab-master\OVF\DMNCTRL\e2e-local-domain-controller-template.ovf'
-
-$vm_name = 'e2e-dmnctl01'
-$vm_cpu = 2
-$vm_memGB = 8
-$vm_diskGB = 50
-########################################################################################
-########################################################################################
 # Initiate Connection to ESXi Host and Deploy the Template
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
-Connect-VIServer -server $esxi_host -User $esxi_user -Password $esxi_pass
-Import-vApp -Source $ovf_source -VMHost $connect_esxi_host
+Connect-VIServer -server $dmnctl_connect_esxi_host -User $dmnctl_connect_esxi_user -Password $dmnctl_connect_esxi_pass
+Import-vApp -Source $dmnctl_ovf_source -VMHost $dmnctl_connect_esxi_host
 
 # Customize the VM
-$pos1 = ($ovf_source.LastIndexOf("\"))+1
-$pos2 = (($ovf_source.Length)-$pos1)-4
-$templateName = $ovf_source.Substring($pos1,$pos2);
+$pos1 = ($dmnctl_ovf_source.LastIndexOf("\"))+1
+$pos2 = (($dmnctl_ovf_source.Length)-$pos1)-4
+$templateName = $dmnctl_ovf_source.Substring($pos1,$pos2);
 
-Set-VM -VM $templateName -NumCpu $vm_cpu -MemoryGB $vm_memGB -Name $vm_name -Confirm $false
+Set-VM -VM $templateName -NumCpu $dmnctl_vm_cpu -MemoryGB $dmnctl_vm_memGB -Name $dmnctl_vm_name -Confirm:$false
 
 # Power on the VM
-Start-VM -VM $vm_name
+Start-VM -VM $dmnctl_vm_name
 
 # Configure DNS
-Invoke-VMScript -ScriptText 'mkdir c:\TESTING123' -VM $vm_name -GuestUser 'administrator' -GuestPassword 'VMware1!'
+#Invoke-VMScript -ScriptText 'mkdir c:\TESTING123' -VM $vm_name -GuestUser 'administrator' -GuestPassword 'VMware1!'
