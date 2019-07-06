6a421f469c0a5eb912b346a87af48fce940192de
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2019-07-05 18:33:03 -0700

    Update build-e2e-lab.ps1

--- powerShell/build-e2e-lab.ps1
+++ powerShell/build-e2e-lab.ps1
@@ -5,32 +5,40 @@
 #                      environments. Use at your own risk. 
 # Date: June 2019
 
+echo "##############################################"
+echo "#######   Starting Build of E2E Lab    #######"
+echo "##############################################"
+
 # Input Parameters from Configuration File
 . "C:\e2e-home-lab-master\e2e-home-lab-master\powerShell\_e2e-lab-configuration-parameters.ps1"
 
 # Initiate Connection to ESXi Host
+echo "Connecting to ESXi Host " $dmnctl_connect_esxi_host
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
 Connect-VIServer -server $dmnctl_connect_esxi_host -User $dmnctl_connect_esxi_user -Password $dmnctl_connect_esxi_pass
 
-# Deploy Windows 12R2 OVF
+# Deploy Domain Controller
+echo "Deploying Domain Controller for e2e.local"
+echo "   1. Deploying Windows OVF Template from source " $dmnctl_ovf_source
 Import-vApp -Source $dmnctl_ovf_source -VMHost $dmnctl_connect_esxi_host
-
-# Customize the VM
-$pos1 = ($dmnctl_ovf_source.LastIndexOf("\"))+1
-$pos2 = (($dmnctl_ovf_source.Length)-$pos1)-4
-$templateName = $dmnctl_ovf_source.Substring($pos1,$pos2);
-
+   $pos1 = ($dmnctl_ovf_source.LastIndexOf("\"))+1
+   $pos2 = (($dmnctl_ovf_source.Length)-$pos1)-4
+   $templateName = $dmnctl_ovf_source.Substring($pos1,$pos2);
 Set-VM -VM $templateName -NumCpu $dmnctl_vm_cpu -MemoryGB $dmnctl_vm_memGB -Name $dmnctl_vm_name -Confirm:$false
-
-# Power on the VM
+echo "   2. Starting VM ...." $dmnctl_vm_name
 Start-VM -VM $dmnctl_vm_name
 Start-Sleep -s 15
-
-# Run DNS Configuraion Scripts
+echo "   3. Creating local pshell directory on VM " $dmnctl_vm_name
 Invoke-VMScript -ScriptText 'mkdir C:\pshell' -VM $dmnctl_vm_name -GuestUser $dmnctl_vm_user -GuestPassword $dmnctl_vm_pass
-Invoke-VMScript -ScriptText 'Copy-Item -Path \\172.16.0.4\drop\* -Destination C:\pshell\' -VM $dmnctl_vm_name -GuestUser $dmnctl_vm_user -GuestPassword $dmnctl_vm_pass
+echo "   4. Copying files...."
+Invoke-VMScript -ScriptText 'Copy-Item -Path \\172.16.0.4\drop\*.ps1 -Destination C:\pshell\' -VM $dmnctl_vm_name -GuestUser $dmnctl_vm_user -GuestPassword $dmnctl_vm_pass
+echo "   5. Configuring network.... " 
 Invoke-VMScript -ScriptText 'C:\pshell\set-DomainController-IP.ps1' -VM $dmnctl_vm_name -GuestUser $dmnctl_vm_user -GuestPassword $dmnctl_vm_pass
+echo "   6. Installing DNS and AD.... " 
 Invoke-VMScript -ScriptText 'C:\pshell\deplou-DomainController-DNS_AD.ps1' -VM $dmnctl_vm_name -GuestUser $dmnctl_vm_user -GuestPassword $dmnctl_vm_pass
+echo "   7. Creating DNS Entries.... " 
+Invoke-VMScript -ScriptText 'C:\pshell\generate-e2e-dns-entries.ps1' -VM $dmnctl_vm_name -GuestUser $dmnctl_vm_user -GuestPassword $dmnctl_vm_pass
+echo "# # # # # # # DOMAIN CONTROLLER SETUP IS COMPLETE # # # # # # # #"
 
 
 
