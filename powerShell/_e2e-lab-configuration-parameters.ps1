a1d5dab20af0343463864d426700a22f40175139
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2019-07-07 11:02:32 -0700

    Update _e2e-lab-configuration-parameters.ps1

--- powerShell/_e2e-lab-configuration-parameters.ps1
+++ powerShell/_e2e-lab-configuration-parameters.ps1
@@ -2,7 +2,8 @@
 #
 #
 
-$dmnctl_connect_esxi_host = ''
+# Domain Controller Parameters
+$dmnctl_connect_esxi_host = '172.16.0.5'
 $dmnctl_connect_esxi_user = 'root'
 $dmnctl_connect_esxi_pass = ''
 $dmnctl_deployto_datastore = 'e2e-esxi01-ds01'
@@ -14,3 +15,12 @@ $dmnctl_vm_memGB = 8
 $dmnctl_vm_diskGB = 50
 $dmnctl_vm_user = 'administrator'
 $dmnctl_vm_pass = ''
+
+# Nested ESXi Parameters
+$nesxi_connect_esxi_host = '172.16.0.6'
+$nesxi_connect_esxi_user = 'root'
+$nesxi_connect_esxi_pass = ''
+$nesxi_deployto_datastore = 'e2e-esxi02-ds01'
+$nesxi_deployto_network = 'VM Network'
+$nesxi_ovf_source = 'C:\e2e-home-lab-master\e2e-home-lab-master\OVF\NESXI\e2e-lab-esxi-6.7.ova'
+$nesxi_namingConvention = 'e2e-nesxi'
