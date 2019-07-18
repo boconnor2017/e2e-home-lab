f0a845b46e828803749cb5a8d9754a5ec3543973
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2019-07-18 07:24:17 -0700

    Update setup-photon-controller.sh

--- shell/setup-photon-controller.sh
+++ shell/setup-photon-controller.sh
@@ -1,4 +1,13 @@
+echo **Installing Python
+cd /usr/local/e2e-home-lab
 yum -y install python3 python-pip 
+echo **Installing vconnector
 git clone https://github.com/dnaeon/py-vconnector.git
 cd py-vconnector
 python3 setup.py install
+mkdir /var/lib/vconnector
+cd /usr/local/e2e-home-lab
+echo **Initializing vconnector database
+vconnector-cli init
+echo **Run vconnector-cli -H esxi02.e2e.local -U root -P <password>
+#vconnector-cli -H esxi02.e2e.local -U root -P p4ssw0rd add
