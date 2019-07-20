efe94c6aea74674331b7c8a21e9bca72cefb9fbf
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2019-07-20 10:30:51 -0700

    Create get-vm-config.py

---
+++ python/get-vm-config.py
@@ -0,0 +1,14 @@
+from pyVim import connect
+
+my_cluster = connect.ConnectNoSSL("172.16.0.6", 443, "root", "BlizzanBandits123")
+
+
+searcher = my_cluster.content.searchIndex
+vm = searcher.FindByIp(ip="172.16.0.19", vmSearch=True)
+print("VM Config Details for 172.16.0.19")
+print(vm.config.name)
+print(vm.config.guestFullName)
+
+# Disconnect from cluster or host
+connect.Disconnect(my_cluster)
+
