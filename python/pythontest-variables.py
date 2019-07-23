ab383c98b879d8783c2ee9edf20faf07f9f39672
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2019-07-23 18:24:57 -0400

    Create pythontest-variables.py

---
+++ python/pythontest-variables.py
@@ -0,0 +1,13 @@
+# These are some comments
+theTruth = True
+theLiars = False
+theStatement = "Python is pretty cool"
+theValue = 100
+
+if theTruth == False:
+  print("The impossible has just happened!")
+if theLiars == True:
+  print("Well that sucks...")
+else:
+  print(theStatement)
+  print(theValue)
