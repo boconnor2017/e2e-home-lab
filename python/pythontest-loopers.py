9e25c08c7abaea0e5987eeb27f963093a40d1e31
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2019-07-23 18:39:39 -0400

    Update pythontest-loopers.py

--- python/pythontest-loopers.py
+++ python/pythontest-loopers.py
@@ -2,7 +2,13 @@
 someCounter = 1
 someMax = 22
 
+print("While....")
 while someCounter <= someMax:
   print(someCounter)
   someCounter +=1
 
+someCounter = 1
+someMax = someMax+1
+print("For....")
+for i in range(someCounter, someMax):
+  print(i)
