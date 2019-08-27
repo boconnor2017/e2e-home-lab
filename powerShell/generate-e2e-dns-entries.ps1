aae438c051f3a30d8c84db33e2e818c690890ae2
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2019-08-27 09:22:49 -0400

    Update generate-e2e-dns-entries.ps1

--- powerShell/generate-e2e-dns-entries.ps1
+++ powerShell/generate-e2e-dns-entries.ps1
@@ -1,5 +1,5 @@
 <#
-Version: 1.0
+Version: 2.0
 Author: Brendan O'Connor (VMWare Professional Services)
 Date: August 2019
 Disclaimer: this solution is not a validated or copywrite solution from VMWare.
@@ -9,49 +9,48 @@ Disclaimer: this solution is not a validated or copywrite solution from VMWare.
 How to use: Edit the values and run this script against a DNS server to automatically create necessary
             DNS entries for VMWare Cloud Foundation. The values should be based on completed information
             in the CloudBuilder spreadsheet. 
-MANAGEMENT WORKLOAD DOMAIN SAMPLE DNS ENTRIES (see VCF Documentation on my.vmware.com for details)
-e2e01m01sddcmgr	e2e.local	172.16.10.60	SDDC Manager
-e2e01m01psc01	e2e.local	172.16.10.61	Platform Services Controller 01
-e2e01m02psc02	e2e.local	172.16.10.62	Platform Services Controller 02
-e2e01m01vc01	e2e.local	172.16.10.63	vCenter Server
-e2e-nesxi01		e2e.local	172.16.10.10	ESXi host 01
-e2e-nesxi02		e2e.local	172.16.10.11	ESXi host 02
-e2e-nesxi03		e2e.local	172.16.10.12	ESXi host 03
-e2e-nesxi04		e2e.local	172.16.10.13	ESXi host 04
-e2e01m01nsx01	e2e.local	172.16.10.64	NSX-V Manager
-VI WORKLOAD DOMAIN #1 (repeat this section for any additional workload domains)
-e2e01w01vc01	e2e.local	172.16.10.68	Additional vCenter Server for VI Workload Domain #1
-e2e01w01nsx01	e2e.local	172.16.10.69	Additional NSX-V Manager for VI Workload Domain #1
-e2e-nesxi05		e2e.local	172.16.10.100	ESXi host 05
-e2e-nesxi06		e2e.local	172.16.10.101	ESXi host 06
-e2e-nesxi07		e2e.local	172.16.10.102	ESXi host 07
-e2e-nesxi08		e2e.local	172.16.10.103	ESXi host 08
-#>
 
 # E2E Lab DNS Entries 172.16.0.0/24 subnet
+# 172.16.0.1: DFGW
+# 172.16.0.2: 
+# 172.16.0.3: TEST Photon VM
 Add-DnsServerResourceRecordA -Name "e2e-control" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.0.4"
 Add-DnsServerResourceRecordA -Name "esxi01" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.0.5"
 Add-DnsServerResourceRecordA -Name "esxi02" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.0.6"
 Add-DnsServerResourceRecordA -Name "e2e-vcenter" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.0.7"
 Add-DnsServerResourceRecordA -Name "e2e-nsx" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.0.8"
+# 172.16.0.9: DNS Server
+# 172.16.0.10: ESG Uplink01
+# 172.16.0.11-13: NSX Controllers
+# 172.16.0.14-16: VTEP IP Pool
 Add-DnsServerResourceRecordA -Name "e2e-cb01" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.0.17"
 Add-DnsServerResourceRecordA -Name "e2e-vcf01" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.0.18"
+Add-DnsServerResourceRecordA -Name "e2e-cb02" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.0.19"
 
 # VCF DNS Entries 172.16.10.0/24 subnet
-Add-DnsServerResourceRecordA -Name "e2e01m01sddcmgr" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.60"
-Add-DnsServerResourceRecordA -Name "e2e01m01psc01" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.61"
-Add-DnsServerResourceRecordA -Name "e2e01m02psc02" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.62"
-Add-DnsServerResourceRecordA -Name "e2e01m01vc01" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.63"
+# 172.16.10.1: DFGW
+# 172.16.10.2: CloudBuilder (DNS entry not required)
+Add-DnsServerResourceRecordA -Name "e2e-vcfjump01" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.3"
+# 172.16.10.4: 
+# 172.16.10.5: 
 Add-DnsServerResourceRecordA -Name "e2e-nesxi01" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.10"
 Add-DnsServerResourceRecordA -Name "e2e-nesxi02" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.11"
 Add-DnsServerResourceRecordA -Name "e2e-nesxi03" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.12"
 Add-DnsServerResourceRecordA -Name "e2e-nesxi04" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.13"
+Add-DnsServerResourceRecordA -Name "e2e01vrli01" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.14"
+Add-DnsServerResourceRecordA -Name "e2e01vrli01a" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.15"
+Add-DnsServerResourceRecordA -Name "e2e01vrli01b" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.16"
+Add-DnsServerResourceRecordA -Name "e2e01vrli01c" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.17"
+# 172.16.10.18-59: 
+Add-DnsServerResourceRecordA -Name "e2e01m01sddcmgr" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.60"
+Add-DnsServerResourceRecordA -Name "e2e01m01psc01" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.61"
+Add-DnsServerResourceRecordA -Name "e2e01w01psc01" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.62"
+Add-DnsServerResourceRecordA -Name "e2e01m01vc01" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.63"
 Add-DnsServerResourceRecordA -Name "e2e01m01nsx01" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.64"
 Add-DnsServerResourceRecordA -Name "e2e01w01vc01" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.68"
 Add-DnsServerResourceRecordA -Name "e2e01w01nsx01" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.69"
-
+# 172.16.10.70-89
 Add-DnsServerResourceRecordA -Name "e2e-nesxi05" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.100"
 Add-DnsServerResourceRecordA -Name "e2e-nesxi06" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.101"
 Add-DnsServerResourceRecordA -Name "e2e-nesxi07" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.102"
 Add-DnsServerResourceRecordA -Name "e2e-nesxi08" -ZoneName "e2e.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.10.103"
-
