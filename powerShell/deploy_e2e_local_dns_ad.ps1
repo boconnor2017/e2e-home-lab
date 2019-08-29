f8e96cfbf38805d6e467944a4056a81346cfa6e5
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2019-08-29 13:40:38 -0400

    Update deploy_e2e_local_dns_ad.ps1

--- powerShell/deploy_e2e_local_dns_ad.ps1
+++ powerShell/deploy_e2e_local_dns_ad.ps1
@@ -2,3 +2,7 @@
 Install-WindowsFeature -Name DNS -IncludeManagementTools
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 Install-ADDSForest -DomainName "e2e.local" -force
+# Set IP Address
+New-NetIPAddress -InterfaceAlias Ethernet0 -IPAddress 172.16.0.9 -PrefixLength 24 -DefaultGateway 172.16.0.1
+#Enable Remote Desktop
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
