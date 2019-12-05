cab7f5a0d9698bf8f457df6414c03e94f2bac18d
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2019-12-04 17:07:47 -0800

    Update step02_a_installK8Packages.sh

--- kubernetes/install/step02_a_installK8Packages.sh
+++ kubernetes/install/step02_a_installK8Packages.sh
@@ -2,6 +2,6 @@ yum -y update
 yum -y install yum-utils
 yum-config-manager --add-repo https://yum.dockerproject.org/repo/main/centos/7
 yum -y update
-yum -y --nogpgcheck install docker-engine-1.12.6-1.el7.centos.x86_64
+yum -y --nogpgcheck install docker-engine-1.11.2-1.el7.centos.x86_64
 service docker start
 yum install -y kubelet kubeadm kubectl
