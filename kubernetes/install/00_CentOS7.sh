0c5fffccdcdab6d86dfe20936458f2dd2402291b
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2019-12-06 08:49:40 -0800

    Update 00_CentOS7.sh

--- kubernetes/install/00_CentOS7.sh
+++ kubernetes/install/00_CentOS7.sh
@@ -8,9 +8,11 @@ curl https://raw.githubusercontent.com/boconnor2017/e2e-home-lab/master/kubernet
 curl https://raw.githubusercontent.com/boconnor2017/e2e-home-lab/master/kubernetes/install/step03_configFW.sh >> step03_configFW.sh
 curl https://raw.githubusercontent.com/boconnor2017/e2e-home-lab/master/kubernetes/install/step04_IPTables.sh >> step04_IPTables.sh
 curl https://raw.githubusercontent.com/boconnor2017/e2e-home-lab/master/kubernetes/install/step05_selinux_swap.sh >> step05_selinux_swap.sh
+curl https://raw.githubusercontent.com/boconnor2017/e2e-home-lab/master/kubernetes/install/step06kubeconfig.sh >> step06kubeconfig.sh
 sh step01_configK8Repo.sh
 sh step02_a_installK8Packages.sh
 sh step02_b_Kubelet.sh
 sh step03_configFW.sh
 sh step04_IPTables.sh
 sh step05_selinux_swap.sh
+sh step06kubeconfig.sh
